b4c156a8b69a7e2376d3655c2e99d9ab2b214c6c
gcecchi <gianluca.cecchi@gmail.com>
2026-06-05 23:27:01 +0200

    use variables corrected

--- variables.tf
+++ variables.tf
@@ -6,10 +6,10 @@ variable "instance_type" {
 
 variable "ami_filter" {
   description = "Name filter and owner for AMI"
-  type = object {{
+  type = object ({
     name = string
     owner = string
-  }}
+  })
 
   default = {
     name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
@@ -19,10 +19,10 @@ variable "ami_filter" {
 
 variable = "environment" {
   description = "Deployment environment"
-  type = object {{
+  type = object ({
     name           = string
     network_prefix = string
-  }}
+  })
   default = {
     name           = "dev"
     network_prefix = "10.0"
