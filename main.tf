7e7c6301bd995f93c406e85c2a605f585f02d144
gcecchi <gianluca.cecchi@gmail.com>
2026-06-05 23:24:00 +0200

    refactor to use variables

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
 data "aws_ami" "app_ami" {
   most_recent = true
 
-
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -12,24 +11,24 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner]
 }
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["eu-south-1a", "eu-south-1b", "eu-south-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["${var.environment.network_prefix}.1.0/24", "${var.environment.network_prefix}.2.0/24", "${var.environment.network_prefix}.3.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   enable_nat_gateway = true
   
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -67,7 +66,7 @@ module "blog_alb" {
   }
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -84,8 +83,8 @@ module "blog_autoscaling" {
 
   name = "blog"
 
-  min_size = 1
-  max_size = 2
+  min_size = var.min_size
+  max_size = var.max_size
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
 
